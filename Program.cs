b7aa4a7d7d2fbeebae398052f9fd4008afa7185c
Kathy Bauer <kbauer17@my.wctc.edu>
2023-03-07 09:21:16 -0600

    [1.13] Input Ticket data, add to Ticket Lists

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿using NLog;
 
 // See https://aka.ms/new-console-template for more information
-string path = Directory.GetCurrentDirectory() + "\\nlog.config";
+string path = Directory.GetCurrentDirectory() + "//nlog.config";
 
 // create instance of Logger
 var logger = LogManager.LoadConfiguration(path).GetCurrentClassLogger();
@@ -28,6 +28,50 @@ do
   if (choice == "1")
   {
     // Add ticket
+    Ticket ticket = new Ticket();
+    StreamWriter sw = new StreamWriter(ticketFilePath);
+            for (int i = 0; i < 7; i++)
+            {
+                // ask a question
+                Console.WriteLine("Enter a ticket (Y/N)?");
+                // input the response
+                string resp = Console.ReadLine().ToUpper();
+                // if the response is anything other than "Y", stop asking
+                if (resp != "Y") { break; }
+                // prompt for a ticket id number
+                Console.WriteLine("Enter a Ticket Number: ");
+                // save the Ticket id number
+                ticket.ticketId = UInt64.Parse(Console.ReadLine());
+                // prompt for ticket Summary
+                Console.WriteLine("Enter a Summary for the ticket:  ");
+                // save the ticket Summary
+                ticket.summary = Console.ReadLine();
+                // prompt for ticket status
+                Console.WriteLine("Enter the ticket status (open/closed).");
+                // save the ticket status
+                ticket.status = Console.ReadLine();
+                // prompt for priority
+                Console.WriteLine("Enter the ticket priority (high/low).");
+                // save the ticket priority
+                ticket.priority = Console.ReadLine();
+                // prompt for the name of the ticket submitter
+                Console.WriteLine("Enter the name of the person submitting the ticket.");
+                // save the name of the submitter
+                ticket.submitter = Console.ReadLine();
+                // prompt for the name of the person to whom the ticket is assigned
+                Console.WriteLine("Enter the name of the person to whom the ticket is assigned.");
+                // save the name of the assignee
+                ticket.assigned = Console.ReadLine();
+                // prompt for the name of the ticket watcher
+                Console.WriteLine("Enter the name of the person watching the ticket.");
+                // save the name of the watcher
+                ticket.watcher = Console.ReadLine();
+                // sw.WriteLine("{0}|{1}|{2}|{3}|{4}|{5}|{6}",ticket.ticketId,ticket.summary, ticket.status,ticket.priority,ticket.submitter,ticket.assigned,ticket.watcher);
+                
+                // add ticket
+              ticketFile.AddTicket(ticket);
+            }
+            sw.Close();
   } else if (choice == "2")
   {
     // Display All Tickets
--- TicketFile.cs
+++ TicketFile.cs
@@ -4,15 +4,15 @@ public class TicketFile
   // public property
   public string filePath { get; set; }
   public List<Ticket> Tickets { get; set; }
+  private static NLog.Logger logger = LogManager.LoadConfiguration(Directory.GetCurrentDirectory() + "//nlog.config").GetCurrentClassLogger();
+
 
   // constructor is a special method that is invoked
   // when an instance of a class is created
   public TicketFile(string ticketFilePath)
   {
     filePath = ticketFilePath;
-    // create instance of Logger
-    NLog.Logger logger = LogManager.LoadConfiguration(Directory.GetCurrentDirectory() + "\\nlog.config").GetCurrentClassLogger();
-
+    
     Tickets = new List<Ticket>();
 
     // to populate the Ticket list with data, read from the data file
@@ -42,6 +42,26 @@ public class TicketFile
       logger.Info("Tickets in file {Count}", Tickets.Count);
     }
     catch (Exception ex)
+    {
+      logger.Error(ex.Message);
+    }
+  }
+
+  // method to add tickets to file
+  public void AddTicket(Ticket ticket)
+  {
+    try
+    {
+      StreamWriter sw = new StreamWriter(filePath, true);
+
+      sw.WriteLine("{0}|{1}|{2}|{3}|{4}|{5}|{6}",ticket.ticketId,ticket.summary, ticket.status,ticket.priority,ticket.submitter,ticket.assigned,ticket.watcher);
+      sw.Close();
+      // add ticket details to Lists
+      Tickets.Add(ticket);
+      // log transaction
+      logger.Info("Ticket id {ticketId} added", ticket.ticketId);
+    } 
+    catch(Exception ex)
     {
       logger.Error(ex.Message);
     }
